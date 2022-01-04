ae21c2187017976dadeefc97b03ed1b65432cc29
anuu <ian.kz.ma@gmail.com>
2022-01-04 12:39:10 -0800

    wip commit setting up merkle component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { connect } from "./redux/blockchain/blockchainActions";
 import { fetchData } from "./redux/data/dataActions";
 import * as s from "./styles/globalStyles";
 import styled from "styled-components";
+import merkleHandler from "./merkleHandler.js";
 
 const truncate = (input, len) =>
   input.length > len ? `${input.substring(0, len)}...` : input;
@@ -270,7 +271,7 @@ function App() {
                         e.preventDefault();
                         dispatch(connect());
                         getData();
-                        getProof();
+                        //getProof();
                       }}
                     >
                       CONNECT
---
+++ src/merkleHandler.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import "./styles/reset.css";
+
+function merkleHandler() {
+    const { MerkleTree } = require('merkletreejs');
+    const keccak256 = require('keccak256');
+
+    
+}
+
+export default merkleHandler;

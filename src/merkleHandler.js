6908483a3f8a2a8b54a6bc4286985519cdfc2a49
unknown <ian.kz.ma@gmail.com>
2022-01-17 15:46:08 -0800

    added new wl for test

--- src/merkleHandler.js
+++ src/merkleHandler.js
@@ -11,9 +11,9 @@ const keccak256 = require('keccak256');
 
 var whitelistAddresses = [
     "0xEC86f9f4034011d7B38D61509110713e76504112", //ALT 3
-    "0xB476128E0605666216c1851499B3af562B0CA0fe",//ALT 2
+    "0xB476128E0605666216c1851499B3af562B0CA0fe", //ALT 2
     "0xd4255a9812E5FAb1C0907E8aA83d5df904C85872", //Mark
-    "xakkadian.eth"
+    "0x1306a37AFa92b3e43C4179CAf1543232ca5cc6e2", //Ty
 ]
 
 const leafNodes = whitelistAddresses.map(addr => keccak256(addr));
